f14c1da0c8a64ea248c6b9902397e3aab2ed22fa
rumencainiao <lgq199508@163.com>
2019-08-16 16:17:05 +0800

    netty8.16日记

--- src/main/java/nettypractice/NettyServer.java
+++ src/main/java/nettypractice/NettyServer.java
@@ -1,13 +1,16 @@
 package nettypractice;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-import javax.sound.sampled.Port;
+import javax.sql.rowset.Predicate;
+import java.sql.Connection;
+
 
 /**
  * netty服务端
@@ -16,23 +19,37 @@ import javax.sound.sampled.Port;
  */
 public class NettyServer {
 
+    public NettyServer start(int port) {
+        Thread thread = new Thread(new TcpServerWork(port));
+        thread.setName("tcp service port monitoring thread -> " + port);
+        thread.start();
+        return this;
+
+    }
+
     private static final class TcpServerWork implements Runnable {
         //
         private NioEventLoopGroup boosGroup;
         //
         private NioEventLoopGroup workerGroup;
+        //
+        private int port;
+
+        TcpServerWork(int port) {
+            this.port = port;
+        }
 
         public void run() {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(boosGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
-                    .option(ChannelOption.SO_BACKLOG, 1024)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
 
                         }
-                    });
+                    }).option(ChannelOption.SO_BACKLOG, 1024);
+            ChannelFuture f = bootstrap.bind(port);
         }
     }
 
